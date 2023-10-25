8c68a4ff1d4f4c4b06fd02014aa0de8f34802a7a
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-10-25 13:31:13 +0200

    started write

--- TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,10 +27,51 @@ namespace TicTacToe
             InitializeComponent();
         }
 
-        private void SetX(object sender, RoutedEventArgs e)
+        int[] datePosition = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        bool isPlaying = true;
+        bool finding = true;
+
+        public void checkWin()
+        {
+            if (n1.Text == "X" && n2.Text == "X" && n3.Text == "X" ||
+                n4.Text == "X" && n5.Text == "X" && n6.Text == "X" ||
+                n7.Text == "X" && n8.Text == "X" && n9.Text == "X" ||
+                n1.Text == "X" && n4.Text == "X" && n7.Text == "X" ||
+                n2.Text == "X" && n5.Text == "X" && n8.Text == "X" ||
+                n3.Text == "X" && n6.Text == "X" && n9.Text == "X" ||
+                n1.Text == "X" && n5.Text == "X" && n9.Text == "X" ||
+                n3.Text == "X" && n5.Text == "X" && n7.Text == "X")
+            {
+                
+            }
+        }
+
+        public void SetX(object sender, RoutedEventArgs e)
         {
             string sourceName = ((FrameworkElement)e.Source).Name;
-            Debug.WriteLine($"{sourceName}");
+            int labelId = int.Parse(Regex.Match(sourceName, @"\d+").Value);
+            var currLabel = (TextBlock)this.FindName(sourceName);
+
+            if (isPlaying)
+            {
+                if (datePosition[labelId - 1] == 0)
+                {
+                    currLabel.Text = "X";
+                    datePosition[labelId - 1] = labelId;
+                    checkWin();
+
+                    if (isPlaying)
+                    {
+                        while (finding)
+                        {
+                            foreach (var i in datePosition)
+                            {
+                                Debug.WriteLine(i);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
